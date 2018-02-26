3bc71f404d68d49b41536dadb4ac2b3a78491127
jiemii <35835853+jiemii@users.noreply.github.com>
2018-02-26 08:12:58 -0800

    Create mouse-input.js

---
+++ mouse-input.js
@@ -0,0 +1,130 @@
+var freqA = 174;
+var freqS = 196;
+var freqD = 220;
+var freqF = 246;
+
+var oscA, oscS, oscD, oscF;
+var playingA, playingS, playingD, playingF;
+var playing = false;
+
+function setup() {
+  backgroundColor = color(255, 0, 255);
+  textAlign(CENTER);
+  
+  oscA = new p5.Oscillator();
+  oscA.setType('triangle');
+  oscA.freq(freqA);
+  oscA.amp(0);
+  oscA.start();
+  
+  oscS = new p5.Oscillator();
+  oscS.setType('triangle');
+  oscS.freq(freqS);
+  oscS.amp(0);
+  oscS.start();
+  
+  oscD = new p5.Oscillator();
+  oscD.setType('triangle');
+  oscD.freq(freqD);
+  oscD.amp(0);
+  oscD.start();
+  
+  oscF = new p5.Oscillator();
+  oscF.setType('triangle');
+  oscF.freq(freqF);
+  oscF.amp(0);
+  oscF.start();
+}
+
+function draw() {
+  if (playing) {
+    background(0, 255, 255);
+  } else {
+    background(255, 0, 255);
+  }
+  text('click here,\nthen press A/S/D/F\n keys to play', width / 2, 40);
+}
+
+function mousePressed(){
+	freqA++;
+  oscA.freq(freqA);
+  freqS++;
+  oscS.freq(freqS);
+  freqD++;
+  oscD.freq(freqD);
+  freqF++;
+  oscF.freq(freqF);
+}
+
+
+function keyPressed() {
+  print("got key press for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+    playingA = true;
+  } else if (key == 'S') {
+    osc = oscS;
+    playingS = true;
+  } else if (key == 'D') {
+    osc = oscD;
+    playingD = true;
+  } else if (key == 'F') {
+    osc = oscF;
+    playingF = true;
+  }
+  if (osc) {
+    osc.amp(0.8, 0.1);
+    playing = true;
+  }
+}
+
+function keyReleased() {
+  print("got key release for ", key);
+  var osc;
+  if (key == 'A') {
+    osc = oscA;
+    playingA = false;
+  } else if (key == 'S') {
+    osc = oscS;
+    playingS = false;
+  } else if (key == 'D') {
+    osc = oscD;
+    playingD = false;
+  } else if (key == 'F') {
+    osc = oscF;
+    playingF = false;
+  }
+  if (osc) {
+    osc.amp(0, 0.1);
+    playingA = false, 
+		playingS = false, 
+		playingD = false,
+		playingF = false;
+  }
+}
+
+function draw() {
+	  background(255);
+	  colorMode(RGB);
+	  if (playingA) {
+	    fill(100, 255, 0);
+	    noStroke();
+	    rect(0, 0, 50, 50);
+	  }
+	  if (playingS) {
+	    fill(150, 50, 255);
+	    noStroke();
+	    rect(50, 50, 50, 50);
+	  }
+	  if (playingD) {
+	    fill(0, 255, 255);
+	    noStroke();
+	    rect(50, 0, 50, 50);
+	  }
+	  if (playingF) {
+	    fill(255, 0, 255);
+	    noStroke();
+	    rect(0, 50, 50, 50);
+	  }
+	}
